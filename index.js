9bc400e627eb361b4426598ae7339b7d788749f0
abdulfrfr <abdulmails112@gmail.com>
2022-12-06 22:59:06 +0100

    loaded

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const express = require("express")
 const axios = require("axios")
+const bodyParser = require("body-parser")
+const { urlencoded } = require("express")
 
-const btn = document.getElementById("submit-btn")
+
+
 
 const app = express()
+app.use(bodyParser.urlencoded({extended:true}))
 
 app.get("/", (req, res)=>{
     res.sendFile(__dirname+"/public/index.html")
-    btn.addEventListener("click", (event)=>{
-        event.preventDefault()
-    })
-        
+      
+})
+
+app.post('/', (req, res)=>{
+    
 })
 
 app.listen(4000, ()=> console.log("working..."))
